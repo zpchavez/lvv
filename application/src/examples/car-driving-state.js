93b482a83e9728cb4ae145245a143fc0c26112ed
Trevor Boone <tdboone@gmail.com>
2015-02-05 08:28:53 -0700

    #1 - added a single colliding box obstacle

--- application/src/examples/car-driving-state.js
+++ application/src/examples/car-driving-state.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Phaser = require('phaser');
+var _      = require('underscore');
 
 var CarDrivingState = function()
 {
@@ -13,6 +14,9 @@ CarDrivingState.prototype.preload = function()
 {
     this.load.image('dirt', 'assets/img/dirt.png');
     this.load.image('car', 'assets/img/bluebox.png');
+    this.load.image('box-black', 'assets/img/black-box.png');
+    this.load.image('box-gray', 'assets/img/gray-box.png');
+    this.load.image('box-brown', 'assets/img/brown-box.png');
 };
 
 CarDrivingState.prototype.create = function()
@@ -22,9 +26,18 @@ CarDrivingState.prototype.create = function()
 
     this.game.physics.startSystem(Phaser.Physics.P2JS);
 
-    this.car = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'car');
+    this.game.physics.restitution = 0.8;
 
+    this.collisionGroup = this.game.physics.p2.createCollisionGroup();
+
+    this.game.physics.p2.updateBoundsCollisionGroup();
+
+    this.car = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'car');
     this.game.physics.p2.enable(this.car);
+    this.car.body.setCollisionGroup(this.collisionGroup);
+    this.car.body.collides(this.collisionGroup);
+
+    makeBoxes(this);
 
     this.cursors = this.game.input.keyboard.createCursorKeys();
 
@@ -46,7 +59,7 @@ CarDrivingState.prototype.update = function()
 
     // apply skid friction
     this.car.body.applyForce(
-        rotateVector(this.car.body.rotation, [carRefVelocity[0] * 0.05, 0]),
+        rotateVector(this.car.body.rotation, [carRefVelocity[0] * 0.2, 0]),
         this.car.body.x,
         this.car.body.y
     );
@@ -60,7 +73,7 @@ CarDrivingState.prototype.update = function()
         );
     } else if (this.cursors.down.isDown) {
         this.car.body.applyForce(
-            rotateVector(this.car.body.rotation, [0, -90]),
+            rotateVector(this.car.body.rotation, [0, -50]),
             this.car.body.x,
             this.car.body.y
         );
@@ -71,6 +84,17 @@ CarDrivingState.prototype.update = function()
     } else if (this.cursors.left.isDown) {
         this.car.body.rotateLeft(80);
     }
+
+    _.each(this.boxes, function(box){
+        box.body.applyForce(
+            [
+                box.body.velocity.x * 0.2,
+                box.body.velocity.y * 0.2
+            ],
+            box.body.x,
+            box.body.y
+        );
+    });
 }
 
 var rotateVector = function(rotation, vector) {
@@ -78,6 +102,19 @@ var rotateVector = function(rotation, vector) {
         vector[0] * Math.cos(rotation) - vector[1] * Math.sin(rotation),
         vector[0] * Math.sin(rotation) + vector[1] * Math.cos(rotation)
     ];
+}
+
+var makeBoxes = function(context) {
+    var box;
+
+    context.boxes = [];
+
+    box = context.add.sprite(400, 400, 'box-gray');
+    context.game.physics.p2.enable(box);
+    box.body.setCollisionGroup(context.collisionGroup);
+    box.body.collides(context.collisionGroup);
+
+    context.boxes.push(box);
 }
 
 module.exports = CarDrivingState;
