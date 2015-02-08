759927de6786573e4ded7325ae897b052bbbf112
Zachary Chavez <zach@rychavez.com>
2015-02-08 08:55:13 -0700

    Refs #2 - Replace literal strings with (pseudo) constants.

--- application/src/objects/car-sprite.js
+++ application/src/objects/car-sprite.js
@@ -45,22 +45,39 @@ Car.prototype.turnLeft = function()
     this.body.rotateLeft(80);
 };
 
+Car.prototype.ROLLING_FRICTION_FORCE = 0.175;
+Car.prototype.SKID_FRICTION_FORCE    = 0.25;
+
 Car.prototype.applyForces = function()
 {
     this.body.setZeroRotation();
 
-    var carRefVelocity = rotateVector(-this.body.rotation, [this.body.velocity.x, this.body.velocity.y]);
+    var carRefVelocity = rotateVector(
+        -this.body.rotation,
+        [this.body.velocity.x, this.body.velocity.y]
+    );
 
     // apply rolling friction
     this.body.applyForce(
-        rotateVector(this.body.rotation, [0, carRefVelocity[1] * 0.175 * this.body.mass]),
+        rotateVector(
+            this.body.rotation,
+            [
+                0,
+                carRefVelocity[1] * this.ROLLING_FRICTION_FORCE * this.body.mass
+            ]
+        ),
         this.body.x,
         this.body.y
     );
 
     // apply skid friction
     this.body.applyForce(
-        rotateVector(this.body.rotation, [carRefVelocity[0] * 0.25 * this.body.mass, 0]),
+        rotateVector(
+            this.body.rotation,
+            [
+                carRefVelocity[0] * this.SKID_FRICTION_FORCE * this.body.mass, 0
+            ]
+        ),
         this.body.x,
         this.body.y
     );
