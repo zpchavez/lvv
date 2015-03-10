8f0be4e0572ef1643be55357cd31330df545a0e7
Trevor Boone <tdboone@gmail.com>
2015-03-10 07:38:38 -0700

    Created example of obstacles layer in tiled

---
+++ application/assets/tilemaps/maps/bathroom-obstacles.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Bathroom Obstacles" tilewidth="827" tileheight="602">
+ <tile id="0">
+  <properties>
+   <property name="Type" value="AspirinBottle"/>
+  </properties>
+  <image width="315" height="551" source="../../img/aspirin-bottle.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Type" value="AspirinPill"/>
+  </properties>
+  <image width="35" height="35" source="../../img/aspirin-pill.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Type" value="Comb"/>
+  </properties>
+  <image width="600" height="127" source="../../img/comb.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Type" value="Floss"/>
+  </properties>
+  <image width="300" height="300" source="../../img/floss.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Type" value="Razor"/>
+  </properties>
+  <image width="189" height="602" source="../../img/razor.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="Type" value="Toothbrush"/>
+  </properties>
+  <image width="827" height="135" source="../../img/toothbrush.png"/>
+ </tile>
+</tileset>
