55d1efe79fac78963c4d151e9a6ea258d90de28b
Trevor Boone <tdboone@gmail.com>
2015-03-11 08:06:25 -0700

    Added image width and height to tsx file and updated track loader to generate objectClasses object with tileset data

--- application/src/objects/track-loader.js
+++ application/src/objects/track-loader.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _         = require('underscore');
 var trackList = require('../../assets/tilemaps/maps/list');
 
 /**
@@ -23,13 +24,32 @@ TrackLoader.prototype.load = function(theme, name, callback)
 
         // Filter out unsupported image collection tilesets and set imagePath
         var tilesets = [];
+
+        var objectClasses    = {};
+        var objectPlacements = [];
+
         data.tilesets.forEach(function (tileset) {
             if (tileset.image) {
                 tileset.imagePath = 'assets/tilemaps/' + tileset.image.replace(/[.\/]*/, '', 'g');
                 tilesets.push(tileset);
+            } else if (tileset.tileproperties) {
+                _.each(tileset.tileproperties, function(tile, index) {
+                    objectClasses[parseInt(tileset.firstgid, 10) + parseInt(index, 10)] = tile;
+                });
             }
         });
         data.tilesets = tilesets;
+
+        console.log('object classes:');
+        console.log(objectClasses);
+
+        console.log('data:');
+        console.log(data);
+
+        // todo: step through all of data.layers and go through all of layer.objects
+        // If object.gid matches something in objectClasses, then set object.type
+        // to objectClass.type, and adjust object.x and object.y using the image dimensions
+        // (objectClass.imageHeight and objectClass.imageWidth) and object.rotation
 
         callback(data);
     });
