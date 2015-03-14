4ea2dabbcb178457a1ec89bc39189bac9b90dcc1
Zachary Chavez <zach@rychavez.com>
2015-03-14 14:35:24 -0700

    Refs #17 - Add map with gravel and pits.

--- application/assets/tilemaps/maps/list.js
+++ application/assets/tilemaps/maps/list.js
@@ -4,7 +4,8 @@ var urlRoot = 'assets/tilemaps/maps/';
 
 module.exports = {
     'Desert' : {
-        'Square Loop' : urlRoot + '/square-loop.tmx',
-        'Figure 8'    : urlRoot + '/figure8.tmx'
+        'Square Loop'     : urlRoot + '/square-loop.tmx',
+        'Figure 8'        : urlRoot + '/figure8.tmx',
+        'Gravel and Pits' : urlRoot + '/gravel-and-pits.tmx'
     }
 };
