915d58b4a8f238e4eb6561cfb9a029467be5153a
Zachary Chavez <zach@rychavez.com>
2015-03-15 16:07:42 -0700

    Refs #17 - Move meta tile to a separate tileset.

---
+++ application/assets/tilemaps/tilesets/desert-with-pit.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="desert-with-pit" tilewidth="32" tileheight="32" spacing="1" margin="1">
+ <image source="../tiles/desert-with-pit.png" width="364" height="199"/>
+ <terraintypes>
+  <terrain name="Sand" tile="38"/>
+  <terrain name="Brick" tile="12"/>
+  <terrain name="Cobblestone" tile="45"/>
+  <terrain name="Gravel" tile="17"/>
+  <terrain name="Pit" tile="20"/>
+ </terraintypes>
+</tileset>
