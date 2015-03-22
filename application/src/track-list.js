6ff148b01879727531ef4fe0c8c7bf0933236fc1
Zachary Chavez <zach@rychavez.com>
2015-03-22 14:34:08 -0700

    Refs #36 - Add track segments.

--- application/src/track-list.js
+++ application/src/track-list.js
@@ -7,6 +7,21 @@ module.exports = {
         'Gravel and Pits'    : urlRoot + 'desert/gravel-and-pits.json',
         'Square Loop'        : urlRoot + 'desert/square-loop.json',
         'Figure 8'           : urlRoot + 'desert/figure8.json',
-        'Bathroom Obstacles' : urlRoot + 'desert/figure8wObs.json'
+        'Bathroom Obstacles' : urlRoot + 'desert/figure8wObs.json',
+        'Two-Segment Track'  : [
+            // Each array at this depth represents a row. This track has only one row of two segments.
+            [
+                // Column 1
+                [
+                    urlRoot + 'desert/two-segment/left-1.json',
+                    urlRoot + 'desert/two-segment/left-2.json'
+                ],
+                // Column 2
+                [
+                    urlRoot + 'desert/two-segment/right-1.json',
+                    urlRoot + 'desert/two-segment/right-2.json'
+                ]
+            ]
+        ]
     }
 };
