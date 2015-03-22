09dcc1f8943e75119376aaca1989bc575d28c85e
Zachary Chavez <zach@rychavez.com>
2015-03-22 16:24:02 -0700

    Refs #36 - Add RNG class.

---
+++ application/src/rng.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var seedrandom = require('seedrandom');
+
+var RNG = function(seed)
+{
+    seed = seed || Date.now();
+
+    this.rng = seedrandom(seed);
+};
+
+RNG.prototype.getIntBetween = function(min, max)
+{
+    return Math.floor(this.rng() * (max - min) + min);
+};
+
+RNG.prototype.pickValueFromArray = function(array)
+{
+    var max, selectedIndex;
+
+    max = array.length - 1;
+
+    selectedIndex = this.getIntBetween(0, max);
+
+    return array[selectedIndex];
+};
+
+module.exports = RNG;
