cb460427597f856cab79eb9dbbdf7c55c565fe42
Trevor Boone <tdboone@gmail.com>
2015-03-25 07:44:23 -0700

    #33 - Created 'countertop sampler' map to try out different background tiles

--- application/assets/tilemaps/maps/list.js
+++ application/assets/tilemaps/maps/list.js
@@ -7,6 +7,7 @@ module.exports = {
         'Gravel and Pits'    : urlRoot + '/desert/gravel-and-pits.json',
         'Square Loop'        : urlRoot + '/desert/square-loop.json',
         'Figure 8'           : urlRoot + '/desert/figure8.json',
-        'Bathroom Obstacles' : urlRoot + 'desert/figure8wObs.json'
+        'Bathroom Obstacles' : urlRoot + 'desert/figure8wObs.json',
+        'Countertop Sampler' : urlRoot + 'desert/countertop-sampler.json'
     }
 };
