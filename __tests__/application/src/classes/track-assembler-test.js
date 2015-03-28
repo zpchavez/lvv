2caf5adce9b98f11876e455f674a5edd65d6394d
Zachary Chavez <zach@rychavez.com>
2015-03-27 21:14:43 -0700

    Refs #36 - Add gulp task for setting up mocha test runner.

---
+++ __tests__/application/src/classes/track-assembler-test.js
@@ -0,0 +1,15 @@
+/* global describe, it, beforeEach */
+/* jshint expr: true */
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('TrackAssembler', function () {
+    beforeEach(function () {
+
+    });
+
+    it('is', function() {
+        expect(2 + 2).to.equal(4);
+    });
+});
