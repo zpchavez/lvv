4c7b44cb1921e4b354103274adbb25665908bc1e
Zachary Chavez <zach@rychavez.com>
2015-04-08 21:10:36 -0700

    Refs #39 - Lower resolution, same aspect ratio, better performance.

--- application/src/main.js
+++ application/src/main.js
@@ -3,4 +3,4 @@
 var Phaser        = require('phaser');
 var MainMenuState = require('./classes/states/menus/main-menu-state');
 
-var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'phaser-template', new MainMenuState());
+var game = new Phaser.Game(960, 540, Phaser.AUTO, 'phaser-template', new MainMenuState());
