aefcf3e89f978c14b0daf4d0c04e08cd87cf2311
Zachary Chavez <zach@rychavez.com>
2016-05-30 10:44:21 -0700

    Start new rectangle-embellish approach

--- application/src/classes/track-generator/desert/desert-generator.js
+++ application/src/classes/track-generator/desert/desert-generator.js
@@ -1,7 +1,6 @@
 var getTemplate = require('./get-desert-template');
 var rng = require('../../../rng');
 var _ = require('underscore');
-var crosses = require('robust-segment-intersect')
 
 var SAND = 39;
 var PAVEMENT = 46;
@@ -39,12 +38,16 @@ var WEST = 270;
 // tiles to leave between the track and the world bounds and other parts of the track
 var TRACK_BUFFER = 5;
 var TRACK_WIDTH  = 5;
+var MAP_SIZE     = 200;
 
 var DesertGenerator = function(options) {
     options = options || {};
 
     this.options = options;
-    this.template = getTemplate();
+    this.template = getTemplate({
+        height: MAP_SIZE,
+        width: MAP_SIZE,
+    });
 };
 
 DesertGenerator.prototype.generate = function() {
@@ -78,10 +81,11 @@ DesertGenerator.prototype._generateBackground = function(points, data) {
         (new Array(200 * 200)).fill(SAND)
     );
 
-    console.log('points', JSON.stringify(points));
-
-    points.forEach(function(point, index) {
-        background.data[point[0] + (point[1] * background.width)] = PAVEMENT;
+    // Draw line between all points, including between the first and last
+    var drawPoints = points.slice();
+    drawPoints.push(points[0]);
+    drawPoints.forEach(function(point, index) {
+        // background.data[point[0] + (point[1] * background.width)] = PAVEMENT;
         if (index > 0) {
             var prevPoint = points[index - 1];
             if ([NORTH, SOUTH].indexOf(prevPoint[2]) !== -1) {
@@ -111,13 +115,46 @@ DesertGenerator.prototype._generateTrackMarkers = function(points, data) {
     var getMarker = function (point, index, isFinish) {
         var id = isFinish ? index : index + 1;
 
+        var adjustment = 5;
+        var markerPoint = [point[0], point[1]];
+        var multiplier;
+        var pointIdx;
+        var prevPoint = isFinish ? points[points.length - 1] : points[index];
+        var headedFromDirection;
+        if (isFinish) {
+            headedFromDirection = point[2];
+        }  else {
+            headedFromDirection = prevPoint[2]
+        }
+        switch (headedFromDirection) {
+            case NORTH:
+                pointIdx = 1;
+                multiplier = isFinish ? -1 : 1;
+                break;
+            case SOUTH:
+                multiplier = isFinish ? 1 : -1;
+                pointIdx = 1;
+                break;
+            case EAST:
+                multiplier = isFinish ? 1 : -1;
+                pointIdx = 0;
+                break;
+            case WEST:
+                multiplier = isFinish ? 1 : -1;
+                pointIdx = 0;
+                break;
+        }
+        markerPoint[pointIdx] = point[pointIdx] + (adjustment * multiplier);
+
         var marker = {
             "id": id,
             "height": this.template.tileheight,
             "width": this.template.tilewidth * TRACK_WIDTH * 3,
-            "x": point[0] * this.template.tilewidth,
-            "y": point[1] * this.template.tileheight,
-            "rotation": point[2],
+            "x": markerPoint[0] * this.template.tilewidth,
+            "y": markerPoint[1] * this.template.tileheight,
+            "rotation": isFinish ? point[2] : (
+                point[2] === 0 ? 270 : point[2] - 90
+            ),
             "visible":true,
         };
 
@@ -138,181 +175,21 @@ DesertGenerator.prototype._generateTrackMarkers = function(points, data) {
         track.objects.push(getMarker(point, index));
     });
 
-    console.log('points', track.objects);
-
     return data;
 };
 
 DesertGenerator.prototype._plotPoints = function() {
+    // Start with a plain rectangle
     var points = [];
-    var corners = [
-        [0, 0],
-        [this.template.width, 0],
-        [this.template.width, this.template.height],
-        [0, this.template.height],
-    ];
-    var awayFromCornerMultipliers = [
-        [1, 1],
-        [-1, 1],
-        [-1, -1],
-        [1, -1],
-    ];
-    var quadrantDirection = [EAST, SOUTH, WEST, NORTH];
-    var startingPoint;
-    var startingQuadrant = rng.getIntBetween(0,3);
-    var corner = corners[startingQuadrant];
-    var multiplier = awayFromCornerMultipliers[startingQuadrant];
 
-    var startingPoint = [
-        rng.getIntBetween(
-            corner[0] + (TRACK_BUFFER * multiplier[0]),
-            corner[0] + ((TRACK_BUFFER + 10) * multiplier[0])
-        ),
-        rng.getIntBetween(
-            corner[1] + (TRACK_BUFFER * multiplier[1]),
-            corner[1] + ((TRACK_BUFFER + 10) * multiplier[1])
-        ),
-        quadrantDirection[startingQuadrant]
-    ];
-    points.push(startingPoint);
-
-    var plotNextPoint = function (retries) {
-        retries = retries || 0;
-        if (retries > 100) {
-            throw new Error('too many retries');
-        }
-
-        var nextPoint;
-        var prevPoints = points.slice().reverse();
-        var nextDirection;
-        switch (prevPoints[0][2]) {
-            case NORTH:
-                if (typeof prevPoints[1] === 'undefined') {
-                    nextDirection = EAST;
-                } else {
-                    nextDirection = rng.pickValueFromArray([EAST, WEST]);
-                }
-                break;
-            case EAST:
-                if (typeof prevPoints[1] === 'undefined') {
-                    nextDirection = SOUTH;
-                } else {
-                    nextDirection = rng.pickValueFromArray([NORTH, SOUTH]);
-                }
-                break;
-            case SOUTH:
-                if (typeof prevPoints[1] === 'undefined') {
-                    nextDirection = WEST;
-                } else {
-                    nextDirection = rng.pickValueFromArray([EAST, WEST]);
-                }
-                break;
-            case WEST:
-                if (typeof prevPoints[1] === 'undefined') {
-                    nextDirection = NORTH;
-                } else {
-                    nextDirection = rng.pickValueFromArray([NORTH, SOUTH]);
-                }
-                break;
-        }
-
-        switch (nextDirection) {
-            case EAST:
-                nextPoint = [
-                    prevPoints[0][0],
-                    rng.getIntBetween(prevPoints[0][1] - 30, TRACK_BUFFER),
-                    EAST,
-                ];
-                break;
-            case WEST:
-                nextPoint = [
-                    prevPoints[0][0],
-                    rng.getIntBetween(prevPoints[0][1] + 30, this.template.height - TRACK_BUFFER),
-                    WEST
-                ];
-                break;
-            case SOUTH:
-                nextPoint = [
-                    rng.getIntBetween(prevPoints[0][0] + 30, this.template.width - TRACK_BUFFER),
-                    prevPoints[0][1],
-                    SOUTH,
-                ];
-                break;
-            case NORTH:
-                nextPoint = [
-                    rng.getIntBetween(prevPoints[0][0] - 30, TRACK_BUFFER),
-                    prevPoints[0][1],
-                    NORTH,
-                ];
-                break;
-        }
-
-        if (this._isValidNextPoint(points, nextPoint)) {
-            console.log(nextPoint, 'is valid');
-            return nextPoint;
-        } else {
-            console.log(nextPoint, 'is not valid');
-            return plotNextPoint(retries + 1);
-        }
-    }.bind(this);
-
-    var loops = 0;
-    while (! this._pointsAreCompletable(points)) {
-        if (loops > 500) {
-            throw new Error('trying too hard');
-        }
-        points.push(plotNextPoint());
-        loops += 1;
-    }
+    points.push(
+        [50, MAP_SIZE - 50, NORTH],
+        [50, 50, EAST],
+        [MAP_SIZE - 50, 50, SOUTH],
+        [MAP_SIZE - 50, MAP_SIZE - 50, WEST]
+    );
 
     return points;
-};
-
-DesertGenerator.prototype._isValidNextPoint = function(points, nextPoint) {
-    // Invalid if goes out of bounds
-    if (
-        nextPoint[0] < TRACK_BUFFER || nextPoint[0] > this.template.width - TRACK_BUFFER ||
-        nextPoint[1] < TRACK_BUFFER || nextPoint[1] > this.template.width - TRACK_BUFFER
-    ) {
-        return false;
-    }
-
-    var lastPoint = points[points.length - 1];
-
-    // Take into account track width by dividing by it
-    var adjustPoint = function (point) {
-        return [
-            Math.round(point[0] / TRACK_WIDTH + TRACK_BUFFER),
-            Math.round(point[1] / TRACK_WIDTH + TRACK_BUFFER),
-            point[2]
-        ];
-    }.bind(this);
-
-    // Invalid if crosses existing track
-    for (var p = 0; p < points.length - 2; p += 1) {
-        // if (crosses(lastPoint, nextPoint, points[p], points[p + 1])) {
-        //     return false;
-        // }
-        var intersects = crosses(
-            adjustPoint(lastPoint),
-            adjustPoint(nextPoint),
-            adjustPoint(points[p]),
-            adjustPoint(points[p + 1])
-        );
-        if (intersects) {
-            return false;
-        }
-    }
-
-    return true;
-};
-
-DesertGenerator.prototype._pointsAreCompletable = function(points) {
-    var lastPoint = points[points.length];
-    var firstPoint = points[0];
-    if (points.length === 8) {
-        return true;
-    }
 };
 
 DesertGenerator.prototype._drawHorizontalLine = function(data, tile, leftPos, length) {
