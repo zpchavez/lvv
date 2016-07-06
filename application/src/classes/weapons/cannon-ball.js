c8400a569bf6cc0a451abc318311528e97d07d1b
Zachary Chavez <zach@rychavez.com>
2016-07-05 20:47:03 -0700

    Cannon hit spins out car instead of making it disappear

--- application/src/classes/weapons/cannon-ball.js
+++ application/src/classes/weapons/cannon-ball.js
@@ -11,20 +11,7 @@ CannonBall.prototype = Object.create(AbstractWeapon.prototype);
 
 CannonBall.prototype.hit = function(car)
 {
-    // Set alpha to 0 because setting visible to false eliminates the player
-    // and we want to give them a chance to respawn and catch-up
-    car.alpha = 0;
-    car.disabled = true;
-    car.body.clearCollision();
-    setTimeout(
-        function() {
-            car.alpha = 1;
-            car.disabled = false;
-            car.addToCollisionGroup(this.state.collisionGroup);
-            this.state.moveCarToLastActivatedMarker(car);
-        }.bind(this),
-        500
-    );
+    car.spinOut();
 }
 
 module.exports = CannonBall;
