cc19352d71ed9c3514bf9cac81fb0f57aa42cefb
Zachary Chavez <zach@rychavez.com>
2016-07-05 21:05:29 -0700

    Commit built project for gh-pages

---
+++ build/assets/tilemaps/tilesets/bathroom-obstacles.tsx
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Bathroom Obstacles" tilewidth="827" tileheight="602">
+ <tile id="0">
+  <properties>
+   <property name="imageHeight" value="551"/>
+   <property name="imageWidth" value="315"/>
+   <property name="type" value="AspirinBottle"/>
+  </properties>
+  <image width="315" height="551" source="../../img/aspirin-bottle.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="imageHeight" value="35"/>
+   <property name="imageWidth" value="35"/>
+   <property name="type" value="AspirinPill"/>
+  </properties>
+  <image width="35" height="35" source="../../img/aspirin-pill.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="imageHeight" value="127"/>
+   <property name="imageWidth" value="600"/>
+   <property name="type" value="Comb"/>
+  </properties>
+  <image width="600" height="127" source="../../img/comb.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="imageHeight" value="300"/>
+   <property name="imageWidth" value="300"/>
+   <property name="type" value="Floss"/>
+  </properties>
+  <image width="300" height="300" source="../../img/floss.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="imageHeight" value="602"/>
+   <property name="imageWidth" value="189"/>
+   <property name="type" value="Razor"/>
+  </properties>
+  <image width="189" height="602" source="../../img/razor.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="imageHeight" value="135"/>
+   <property name="imageWidth" value="827"/>
+   <property name="type" value="Toothbrush"/>
+  </properties>
+  <image width="827" height="135" source="../../img/toothbrush.png"/>
+ </tile>
+</tileset>
---
+++ build/assets/tilemaps/tilesets/meta-tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="meta-tile" tilewidth="32" tileheight="32">
+ <image source="../tiles/meta-tile.png" width="32" height="32"/>
+</tileset>
